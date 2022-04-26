af9f43f1fd714bf3ff7ddc5afc349981b3323c73
Carlos Jesus Perez Dugarte <soycarlosdugarte@gmail.com>
2022-04-26 07:37:16 -0300

    Se crean configuraciones iniciales

--- next.config.js
+++ next.config.js
@@ -6,13 +6,7 @@ const config = {
   future: {
     webpack5: true,
   },
-  env: {
-    spaceId: 'x3wnj8s68ndy',
-    managementToken: 'CFPAT-p2Bknm50fCizA6Mi6NZzWbspcfYvj3Hw3qra_appFzU',
-    urlApiBase: 'https://graphql.contentful.com/content/v1/spaces/',
-    contentDeliveryApi: 'zg0Zd7tDTRm1YLGIILFyKDAFRn6JzGKHf7tME-SdA0Y',
-    contentPreviuApi: 'JNTQ1y792NyaapxhYNXtyknEGzWfO4_BadSl024hrRc',
-  },
+
 }
 
 module.exports = withBundleAnalyzer(config)
